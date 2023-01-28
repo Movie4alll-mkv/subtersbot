a8be3cf68a08c3e6bb7da0d6227a5039c4c2c7a4
meharfaizan <87762660+meharfaizan@users.noreply.github.com>
2023-01-28 19:31:53 +0500

    Update config.py

--- config.py
+++ config.py
@@ -3,21 +3,21 @@ import os
 
 class Config:
 
-    BOT_TOKEN = os.environ.get('BOT_TOKEN', None)
-    APP_ID = os.environ.get('APP_ID', None)
-    API_HASH = os.environ.get('API_HASH', None)
+    BOT_TOKEN = os.environ.get('BOT_TOKEN', '5442493323:AAHPw8TNe0hh2zCAQKm_2O2o6KdmQ3Okgf8')
+    APP_ID = os.environ.get('APP_ID', '6534707')
+    API_HASH = os.environ.get('API_HASH', '4bcc61d959a9f403b2f20149cbbe627a')
 
     #comma seperated user id of users who are allowed to use
 
     DOWNLOAD_DIR = 'downloads'
     OWNER_ID = int(os.environ.get("OWNER_ID", 1316963576))
-    UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", None)  
+    UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", 'animecolony')  
     # your telegram id
-    OWNER_ID = int(os.environ.get("OWNER_ID", ""))
+    OWNER_ID = int(os.environ.get("OWNER_ID", "1430593323"))
     # database session name, example: xurluploader
-    SESSION_NAME = os.environ.get("SESSION_NAME", "")
+    SESSION_NAME = os.environ.get("SESSION_NAME", "hpbot")
     # database uri (mongodb)
-    DATABASE_URL = os.environ.get("DATABASE_URL", "")
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-100"))
+    DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://Uploader:Uploader@cluster0.ba0ppxa.mongodb.net/?retryWrites=true&w=majority")
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001843564893"))
     BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", "False"))
     DOWNLOAD_LOCATION = "./DOWNLOADS"
