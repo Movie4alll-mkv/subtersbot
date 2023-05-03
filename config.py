c7925fcb33ef8b567a43c9c5073f67674e3308e8
gjjif <105107960+gjjif@users.noreply.github.com>
2023-05-03 11:33:41 +0500

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 
 class Config:
 
-    BOT_TOKEN = os.environ.get('BOT_TOKEN', '5442493323:AAHPw8TNe0hh2zCAQKm_2O2o6KdmQ3Okgf8')
+    BOT_TOKEN = os.environ.get('BOT_TOKEN', '')
     APP_ID = os.environ.get('APP_ID', '6534707')
     API_HASH = os.environ.get('API_HASH', '4bcc61d959a9f403b2f20149cbbe627a')
 
