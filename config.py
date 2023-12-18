15dac1f6a2db3387feae039ca4f5acf2b19a59de
Movie4alll-mkv <77849768+Movie4alll-mkv@users.noreply.github.com>
2023-12-18 18:54:19 +0500

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 
 class Config:
 
-    BOT_TOKEN = os.environ.get('BOT_TOKEN', '5442493323:AAHGeguKxL1ZjBzbk8AtDa2hSpk-d77hcK8')
+    BOT_TOKEN = os.environ.get('BOT_TOKEN', '')
     APP_ID = os.environ.get('APP_ID', '6534707')
     API_HASH = os.environ.get('API_HASH', '4bcc61d959a9f403b2f20149cbbe627a')
 
